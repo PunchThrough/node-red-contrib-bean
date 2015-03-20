bdad81d269c6e363a4f08124d965419c466d98a1
Ray Kampmeier <raykampmeier@gmail.com>
2015-03-20 14:22:09 -0700

    - Implemented timeout mode for serial node - Bean connection mode now has a default - Added clarification to the serial node help text

--- Bean/beanSerial.js
+++ Bean/beanSerial.js
@@ -45,16 +45,23 @@ module.exports = function(RED) {
         this.splitChar = this.newline.replace("\\n","\n").replace("\\r","\r").replace("\\t","\t").replace("\\e","\e").replace("\\f","\f").replace("\\0","\0").charCodeAt(0);
 
         this.rxBuffer = new Buffer(0);
+        this.serialTimeout = null;
 
         // respond to inputs....
         this.on('input', function (msg) {
-            // in this example just send it straight on... should process it here really
-            //this.send(msg);
             if(this.beanConfig){
+                // This works for both String and Buffer objects. Strings are expected to be utf8 encoded
                 this.beanConfig.write(new Buffer(msg.payload), function(){
                 })
             }
         });
+
+        var sendIncomingBuffer = function(buffer){
+            var msg = {};
+            msg.topic = "serial";
+            msg.payload = (this.bin === true ? buffer : buffer.toString());
+            this.send(msg);
+        }.bind(this)
 
         var attemptToPopCharSeparatedMessage = function(){
             var i = 0;
@@ -64,11 +71,7 @@ module.exports = function(RED) {
                     var outputBuf;
                     // Include the message separation character?
                     outputBuf = this.rxBuffer.slice(0,(this.addchar ? i+1 : i));
-                    var msg = {};
-                    msg.topic = "serial";
-                    msg.payload = (this.bin === true ? outputBuf : outputBuf.toString());
-                    this.send(msg);
-
+                    sendIncomingBuffer(outputBuf);
                     this.rxBuffer = this.rxBuffer.slice(i+1);
                     return true;
                 }
@@ -86,11 +89,7 @@ module.exports = function(RED) {
             }
 
             outputBuf = this.rxBuffer.slice(0,separationLength);
-            var msg = {};
-            msg.topic = "serial";
-            msg.payload = (this.bin === true ? outputBuf : outputBuf.toString());
-            this.send(msg);
-
+            sendIncomingBuffer(outputBuf);
             this.rxBuffer = this.rxBuffer.slice(separationLength);
             return true;
         }.bind(this)
@@ -107,7 +106,16 @@ module.exports = function(RED) {
                     while(attemptToPopCharSeparatedMessage());
                     break;
                 case 'time':
-                    // TODO
+                    if(this.serialTimeout === null){
+                        var timeout = parseInt(this.newline);
+                        timeout = (timeout === NaN) ? 0 : timeout;
+
+                        this.serialTimeout = setTimeout(function(){
+                            sendIncomingBuffer(this.rxBuffer);
+                            this.rxBuffer = new Buffer(0);
+                            this.serialTimeout = null;
+                        }.bind(this),timeout);
+                    }
                     break;
                 case 'count':
                     while(attemptToPopLengthSeparatedMessage());
@@ -123,6 +131,7 @@ module.exports = function(RED) {
         }.bind(this));
 
         this.on("close", function(done) {
+            clearTimeout(this.serialTimeout);
             done();
         });
 
